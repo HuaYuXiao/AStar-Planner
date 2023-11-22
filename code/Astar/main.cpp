df1492a3ceeba69cf7f0e923279a3649078eeca2
Yuxiao Hua <1628280289@qq.com>
2023-11-22 09:45:02 +0800

    Changes

--- code/Astar/main.cpp
+++ code/Astar/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
- 
 #include "Astar.h"
- 
-bool InPath(const int &row, const int &col, const std::list<Point *> &path) {
+
+using namespace std;
+
+
+bool InPath(const int &row, const int &col, const list<Point *> &path) {
   for (const auto &p : path) {
     if (row == p->x && col == p->y) {
       return true;
@@ -13,7 +15,7 @@ bool InPath(const int &row, const int &col, const std::list<Point *> &path) {
  
 int main() {
   //初始化地图，用二维矩阵代表地图，1表示障碍物，0表示可通
-  std::vector<std::vector<int>> map = {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  vector<vector<int>> map = {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
                                        {1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1},
                                        {1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
                                        {1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1},
@@ -29,27 +31,27 @@ int main() {
   Point end(6, 10);
  
   // A*算法找寻路径
-  std::list<Point *> path = astar.GetPath(start, end, false);
+  list<Point *> path = astar.GetPath(start, end, false);
  
   // 打印结果
   for (auto &p : path) {
-    std::cout << "(" << p->x << "," << p->y << ") ";
+    cout << "(" << p->x << "," << p->y << ") ";
   }
-  std::cout << "\n";
+  cout << "\n";
  
   for (int row = 0; row < map.size(); ++row) {
     for (int col = 0; col < map[0].size(); ++col) {
       if (InPath(row, col, path)) {
         if (map[row][col] != 0) {
-          std::cout << "e ";
+          cout << "e ";
         } else {
-          std::cout << "* ";
+          cout << "* ";
         }
       } else {
-        std::cout << map[row][col] << " ";
+        cout << map[row][col] << " ";
       }
     }
-    std::cout << "\n";
+    cout << "\n";
   }
   return 0;
 }
