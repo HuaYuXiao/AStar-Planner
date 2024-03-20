1b375b430baa5ffe2f6a3334d54454dbfc976a6e
Eason Hua <1628280289@qq.com>
2024-03-20 21:58:59 +0800

    Upgrade tf to tf2_ros

--- include/tools.h
+++ include/tools.h
@@ -13,6 +13,14 @@
 
 #include <Eigen/Eigen>
 
+#include <pcl_ros/point_cloud.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <tf/transform_listener.h>
 #include <tf/transform_broadcaster.h>
 #include <tf2_ros/static_transform_broadcaster.h>
 
@@ -24,10 +32,13 @@
 #include <sensor_msgs/LaserScan.h>
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/Path.h>
+#include <nav_msgs/OccupancyGrid.h>
 #include "prometheus_msgs/PositionReference.h"
 #include "prometheus_msgs/Message.h"
 #include "prometheus_msgs/DroneState.h"
 #include "prometheus_msgs/ControlCommand.h"
+#include <visualization_msgs/Marker.h>
+
 
 #include "occupy_map.h"
 
